e3ae304d2925bba3a486a6241845c08c6c52e812
niksw7 <nikeshshetty@hotmail.com>
2012-07-23 16:56:18 +0530

    Update recursivelyzip.php

---
+++ recursivelyzip.php
@@ -0,0 +1,27 @@
+  function unzipfiles($srczip,$destzip)
+{
+  
+$zip = new ZipArchive;
+     $res = $zip->open($srczip);
+     if ($res === TRUE) {
+         $zip->extractTo($destzip);// c:\do
+         $zip->close();
+		 $filelist=scandir($destzip,1);
+		 foreach($filelist as $file)
+		{
+			//echo 'insid foreach    '.'=='.substr($file,-3,3).'<br/>';
+		if(substr($file,-3,3)=="zip")
+		{
+			
+			//echo 'src1='."$destzip.'/'.$file".'<br/>dest1='."$destzip/".substr($file , 0, strlen($file)-4  );
+			unzipfiles($destzip.'/'.$file, $destzip.'/'.substr($file , 0, strlen($file)-4  ) ) ;
+			
+			}	
+		} 
+		 
+		// print_r($filelist);
+         return 1;
+     } else {
+         return 0;
+     }
+}
